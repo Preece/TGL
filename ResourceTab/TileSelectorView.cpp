d8072d3893e4dc1e9e6c3e1090287da6c928ae13
Casey Clyde <caseysclyde@gmail.com>
2013-01-07 20:16:25 -0800

    Multiple tiles can now be correctly selected in the tile selector

--- ResourceTab/TileSelectorView.cpp
+++ ResourceTab/TileSelectorView.cpp
@@ -2,19 +2,46 @@
 
 TileSelectorView::TileSelectorView(QObject *parent)
 {
+    selection = new QRubberBand(QRubberBand::Rectangle, this);
+}
+
+TileSelectorView::~TileSelectorView()
+{
+    delete selection;
 }
 
 void TileSelectorView::mousePressEvent(QMouseEvent *event)
 {
     QGraphicsView::mousePressEvent(event);
+
+    if(event->button() == Qt::LeftButton)
+    {
+        clickSpot = event->pos();
+    }
 }
 
 void TileSelectorView::mouseMoveEvent(QMouseEvent *event)
 {
     QGraphicsView::mouseMoveEvent(event);
+
+    if(event->buttons() & Qt::LeftButton)
+    {
+        selection->setGeometry(clickSpot.x(), clickSpot.y(),
+                               event->pos().x() - clickSpot.x(), event->pos().y() - clickSpot.y());
+
+        QPainterPath path;
+        path.addRect(clickSpot.x(), clickSpot.y(),
+                     event->pos().x() - clickSpot.x(), event->pos().y() - clickSpot.y());
+        this->scene()->setSelectionArea(path);
+    }
 }
 
 void TileSelectorView::mouseReleaseEvent(QMouseEvent *event)
 {
     QGraphicsView::mouseReleaseEvent(event);
+
+    if(event->button() == Qt::LeftButton)
+    {
+        selection->setGeometry(0, 0, 0, 0);
+    }
 }
--- ResourceTab/TileSelectorView.h
+++ ResourceTab/TileSelectorView.h
@@ -2,12 +2,16 @@
 #define TILESELECTORVIEW_H
 
 #include <QGraphicsView>
+#include <QRubberBand>
+#include <QPoint>
+#include <QMouseEvent>
 
 class TileSelectorView : public QGraphicsView
 {
     Q_OBJECT
 public:
     explicit TileSelectorView(QObject *parent = 0);
+    ~TileSelectorView();
 
     void mousePressEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
@@ -16,6 +20,10 @@ public:
 signals:
     
 public slots:
+
+private:
+    QRubberBand *selection;
+    QPoint clickSpot;
     
 };
 
