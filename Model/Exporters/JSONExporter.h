da0a683db5510bfa69a965bdcbc88704528b6708
Preece <caseysclyde@gmail.com>
2014-04-03 12:42:03 -0700

    Create JSONExporter.h

---
+++ Model/Exporters/JSONExporter.h
@@ -0,0 +1,13 @@
+#include <QString>
+
+#include "../Exporter.h"
+#include "../ResourceManager.h"
+
+class JSONExporter : public Exporter
+{
+    JSONExporter();
+    ~JSONExporter();
+    
+    bool Export(ResourceManager *resources, QString filename);
+    bool Import(ResourceManager *resources, QString filename);
+};
