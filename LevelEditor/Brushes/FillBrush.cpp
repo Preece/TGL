9b5efddc98a3dbcfe5a9f286d427d36c0e545968
Casey Clyde <caseysclyde@gmail.com>
2014-04-12 09:13:57 -0700

    New non-recursive fill algorithm. Still slow and crusty as shit

--- LevelEditor/Brushes/FillBrush.cpp
+++ LevelEditor/Brushes/FillBrush.cpp
@@ -35,6 +35,7 @@ void FillBrush::Fill(int tileX, int tileY, TileCoord newOrigin, TileCoord oldOri
        tileX < 0 || tileY < 0)
            return;
 
+    /*
     //if the current tile is of the type to be replaced
     if(newLayer->GetTileOrigin(tileX, tileY) == oldOrigin)
     {
@@ -46,5 +47,42 @@ void FillBrush::Fill(int tileX, int tileY, TileCoord newOrigin, TileCoord oldOri
         Fill(tileX + 1, tileY, newOrigin, oldOrigin, newLayer);
         Fill(tileX, tileY - 1, newOrigin, oldOrigin, newLayer);
         Fill(tileX, tileY + 1, newOrigin, oldOrigin, newLayer);
+    }
+    */
+
+    // 2. Set Q to the empty queue.
+    QList<TileCoord> seedQueue;
+
+    // 3. Add node to the end of Q.
+    seedQueue.push_back(TileCoord(tileX, tileY));
+
+    // 4. While Q is not empty:
+    while(!seedQueue.isEmpty())
+    {
+        // 5.     Set n equal to the last element of Q.
+        TileCoord tempCoord = seedQueue.back();
+
+        // 6.     Remove last element from Q.
+        seedQueue.removeLast();
+
+        //if the position is beyond the bounds of the scene, ignore it
+        if(tempCoord.first >= newLayer->GetLayerWidth() ||
+           tempCoord.second >= newLayer->GetLayerHeight() ||
+           tempCoord.first < 0 || tempCoord.second < 0)
+               continue;
+
+        // 7.     If the color of n is equal to target-color:
+        if(newLayer->GetTileOrigin(tempCoord.first, tempCoord.second) == oldOrigin)
+        {
+            // 8.         Set the color of n to replacement-color.
+            //replace this tile with the new type
+            newLayer->ModifyTileItem(tempCoord.first, tempCoord.second, newOrigin);
+
+            seedQueue.push_back(TileCoord(tempCoord.first - 1, tempCoord.second));
+            seedQueue.push_back(TileCoord(tempCoord.first + 1, tempCoord.second));
+            seedQueue.push_back(TileCoord(tempCoord.first, tempCoord.second - 1));
+            seedQueue.push_back(TileCoord(tempCoord.first, tempCoord.second + 1));
+        }
+    // 13. Return.
     }
 }
